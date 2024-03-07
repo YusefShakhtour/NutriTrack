d48036253f36be6fdd078deca94d654a199be32d
Eman Amodia <etamodia@ncsu.edu>
2024-03-07 03:55:13 -0500

    [Frontend Routes]

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -6,6 +6,19 @@ const PORT = process.env.PORT;
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + '/static'));
 
+const html_dir = __dirname + '/static/';
+
+app.get('/login', (req, res) => {
+  res.sendFile(`${html_dir}login.html`);
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(`${html_dir}dashboard.html`);
+});
+
+app.get('/addRecipe', (req, res) => {
+  res.sendFile(`${html_dir}addRecipe.html`);
+});
 
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
