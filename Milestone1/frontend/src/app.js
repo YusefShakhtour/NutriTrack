34aa410e83593581bde9409d15571e6cb964fc7c
nour amara <namara@ncsu.edu>
2024-03-06 20:38:53 -0500

    change default frontend route

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Designate the static folder as serving static resources
-app.use(express.static(__dirname + '/static/addRecipe.html'));
+app.use(express.static(__dirname + '/static'));
 
 
 // As our server to listen for incoming connections
