5be18fea8a425f6c994d2d283f40ef8e58fb15d5
nour amara <namara@ncsu.edu>
2024-04-04 00:28:17 -0400

    add the middleware to be used to get the token

--- Milestone2/api/src/APIRoutes.js
+++ Milestone2/api/src/APIRoutes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-
+const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
 const { users, recipes } = require('./data');
+
+router.use(cookieParser());
+
+const {TokenMiddleware, generateToken, removeToken} = require('../../middleware/TokenMiddleware');
+
 
 // Default
 router.get('/', (req, res) => {
@@ -9,18 +15,66 @@ router.get('/', (req, res) => {
 
 // Login
 router.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  const user = Object.values(users).find(
-    (u) => u.email === email && u.password === password
-  );
-
-  if (user) {
-    res.json({ success: true, message: 'Login successful' });
-  } else {
-    res.json({ success: false, message: 'Invalid credentials' });
+  if (req.body.username && req.body.password) {
+    console.log("Login attempt with username: " + req.body.username + " and password: " + req.body.password)
+    getUserByCredentials(req.body.username, req.body.password)
+      .then(user => {
+        console.log("User logged in successfully:", user);
+        let result ={
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          recipe: user.recipe
+        }
+        generateToken(req, res, user);
+        res.json(result);
+      })
+      .catch(error => {
+        res.status(error.code).json({ error: error.message });
+      });
   }
-});
+}); 
+   
+   
+
+  
+function getUserByCredentials(username, password) {
+  console.log("Looking for user: ", username );
+  return new Promise((resolve, reject) => {
+    const user = Object.values(users).find(user => user.username == username);
+    console.log("User found: ", user ? user.username : "none");
+    if (user) { // we found our user
+      crypto.pbkdf2(password, user.salt, 100000, 64, 'sha512', (err, derivedKey) => {
+        if (err) { //problem computing digest, like hash function not available
+          reject({code: 400, message: "Error: " +err});
+        }
+
+        const digest = derivedKey.toString('hex');
+        if (user.password == digest) {
+          resolve(getFilteredUser(user));
+        }
+        else {
+          reject({code: 401, message: "Invalid username or password"});
+        }
+      });
+    }
+    else { // if no user with provided username
+      reject({code: 401, message: "No such user"});
+    }
+  });
+
+} 
+
+function getFilteredUser(user) {
+  return {
+    "id": user.id,
+    "username": user.username,
+    "email": user.email,
+    "recipe": user.recipe
+  }
+}
+
+
 
 // Create user
 router.post('/users', (req, res) => {
