d5517206d72c3da35f7d09ff3fd6840dffcc86cf
Eman Amodia <etamodia@ncsu.edu>
2024-04-17 00:45:02 -0400

    Meals functionality
    Co-authored-by: Yusef Feras Shakhtour <yfshakht@ncsu.edu>
    Co-authored-by: amaranour <amaranour@users.noreply.github.com>

--- Final/frontend/src/static/js/dashboard.js
+++ Final/frontend/src/static/js/dashboard.js
@@ -18,8 +18,8 @@ api.getCurrentUser().then(user => {
     //Loop through all user meals and add them to frontend
     api.getDailyMeals(localStorage.getItem("userId")).then(meals => {
         localStorage.setItem("dailyMeals", JSON.stringify(meals));
-        // console.log(meals);
-        // console.log(meals.length);
+        console.log(meals);
+        console.log(meals.length);
 
         for (let i = 0; i < meals.length; i++) {
             // console.log(meals[i]);
@@ -48,12 +48,50 @@ let mealItem = document.querySelector(".mealItem");
 // });
 
 
+let recContainer = document.querySelector(".recContainer");
+recContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("recItem")) {
+        console.log("click");
+        let recipes = JSON.parse(localStorage.getItem("recipes"));
+        recipes.forEach(recipe => {
+            console.log("RecipeName: " + recipe.name);
+            console.log("InnerHTML: " + e.target.innerHTML);
+            if (recipe.name == e.target.innerHTML) {
+                console.log("Found recipe");
+                console.log("Creating Meal");
+                api.createMeal(localStorage.getItem("userId"), formatDate(), recipe.rec_id);
+                window.location.reload();
+            }
+        });
+    }
+});
+
+
 //Add recipe HTML
 function addRecipe(recipe) {
     let newRecipe = document.createElement("li");
     newRecipe.classList.add("recItem");
     newRecipe.innerHTML = recipe.name;
     recipeList.append(newRecipe);
+}
+
+function formatDate() {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+    let day = ('0' + currentDate.getDate()).slice(-2);
+    let formattedDate = year + '-' + month + '-' + day;
+    return formattedDate;
+}
+
+function formatDatePrevious(daysPast) {
+    let currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() - 24 * daysPast);
+    let year = currentDate.getFullYear();
+    let month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+    let day = ('0' + currentDate.getDate()).slice(-2);
+    let formattedDate = year + '-' + month + '-' + day;
+    return formattedDate;
 }
 
 function addMeal(meal) {
@@ -76,60 +114,137 @@ function addMeal(meal) {
     consumptionList.append(newMeal);
 }
 
+// when one of the meals is clicked, it will be deleted
+
+consumptionList.addEventListener('click', function (e) {
+    let meal = e.target;
+    let mealName = meal.innerHTML;
+
+    // Retrieve meals from local storage
+    const mealsJSON = localStorage.getItem('dailyMeals');
+    const meals = JSON.parse(mealsJSON);
+
+    meals.forEach(meal => {
+        // console.log(meal);
+        // console.log(mealName);
+        if (mealName == meal.name) {
+            // console.log(meal.meals_id);
+            api.deleteMeal(meal.meals_id).then(() => {
+                meal.remove();
+            });
+        }
+    });
+});
+
+
+
 /**
  * CHART CREATION
  */
-
 // Pie chart
-(async function () {
-    const xValues = ["Fat", "Protein", "Carbohydrates"];
-    const yValues = [];
-    const barColors = ["red", "green", "blue"];
+const xValues = ["Fat", "Protein", "Carbohydrates"];
+const yValues = [];
+const barColors = ["red", "green", "blue"];
 
-    let totalFat = 0;
-    let totalProtein = 0;
-    let totalCarbs = 0;
+let totalFat = 0;
+let totalProtein = 0;
+let totalCarbs = 0;
 
-    // Retrieve meals from local storage
-    const mealsJSON = localStorage.getItem('weeklyMeals');
-    const meals = JSON.parse(mealsJSON);
+// Retrieve meals from local storage
+const mealsJSON = localStorage.getItem('weeklyMeals');
+const meals = JSON.parse(mealsJSON);
 
-    // Retrieve recipes from local storage
-    const recipesJSON = localStorage.getItem('recipes');
-    const recipes = JSON.parse(recipesJSON);
+// Retrieve recipes from local storage
+const recipesJSON = localStorage.getItem('recipes');
+const recipes = JSON.parse(recipesJSON);
 
-    // Assuming meals and recipes are not null
-    meals.forEach(meal => {
-        recipes.forEach(recipe => {
-            if (meal.rec_id == recipe.rec_id) {
-                totalFat += recipe.fat;
-                totalProtein += recipe.protein;
-                totalCarbs += recipe.carbs;
-            }
-        });
+// Assuming meals and recipes are not null
+meals.forEach(meal => {
+    recipes.forEach(recipe => {
+        if (meal.rec_id == recipe.rec_id) {
+            totalFat += recipe.fat;
+            totalProtein += recipe.protein;
+            totalCarbs += recipe.carbs;
+        }
     });
+});
 
-    yValues.push(totalFat);
-    yValues.push(totalProtein);
-    yValues.push(totalCarbs);
+yValues.push(totalFat);
+yValues.push(totalProtein);
+yValues.push(totalCarbs);
 
-    new Chart(
-        document.getElementById('distribution'),
-        {
-            type: "pie",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: "Weekly Distribution of Macro-nutrients"
-                }
+new Chart(
+    document.getElementById('distribution'),
+    {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Weekly Distribution of Macro-nutrients"
             }
         }
-    );
-})();
+    }
+);
+
+
+
+// Stats
+const fat_goal = [];
+const protein_goal = [];
+const carb_goal = [];
+await api.getUserStats(localStorage.getItem("userId")).then(stats => {
+    for (let x = 0; x < 7; x++) {
+        fat_goal.push(stats[0].fat_goal);
+        protein_goal.push(stats[0].protein_goal);
+        carb_goal.push(stats[0].carb_goal);
+    }
+});
+
+// Fat chart
+const L7fat = [];
+let daysFat = 0;
+
+for (let x = 0; x < 7; x++) {
+    meals.forEach(meal => {
+        // console.log(formatDatePrevious(x));
+        // console.log(meal.date);
+        if (meal.date == formatDatePrevious(x)) {
+            // console.log("Meal Date: " + meal.date);
+            recipes.forEach(recipe => {
+                if (meal.rec_id == recipe.rec_id) {
+                    // console.log("Recipe Fat: " + recipe.fat);
+                    daysFat += recipe.fat;
+                }
+            });
+        }
+    });
+    L7fat.push(daysFat);
+    daysFat = 0;
+}
+
+
+
+// Protein chart
+const L7protein = [];
+let daysProtein = 0;
+
+for (let i = 0; i < 7; i++) {
+    meals.forEach(meal => {
+        if (meal.date == formatDatePrevious(i)) {
+            recipes.forEach(recipe => {
+                if (meal.rec_id == recipe.rec_id) {
+                    daysProtein += recipe.protein
+                }
+            });
+        }
+    });
+    L7protein.push(daysProtein);
+    daysProtein = 0;
+}
